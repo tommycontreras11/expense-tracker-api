01dc23be9a7acea92de1afb00ed71c44df578736
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2026-07-07 22:23:39 -0400

    fix: remove debug console log from data source configuration

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,8 +6,6 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-console.log("dirname: ", path.join(__dirname, "entities/*.entity.{.ts, .js}"))
-
 const AppDataSource = new DataSource({
     type: "mysql",
     host: config.DB_HOST,
