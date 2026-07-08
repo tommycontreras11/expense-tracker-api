befa1357de13236dc95f636692f8a3226d1ca128
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2026-07-07 22:22:48 -0400

    fix: update TypeScript version to 5.9.2 and add migration scripts to package.json

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,7 +6,9 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-export const AppDataSource = new DataSource({
+console.log("dirname: ", path.join(__dirname, "entities/*.entity.{.ts, .js}"))
+
+const AppDataSource = new DataSource({
     type: "mysql",
     host: config.DB_HOST,
     port: config.DB_PORT,
@@ -14,8 +16,8 @@ export const AppDataSource = new DataSource({
     username: config.DB_USER,
     password: config.DB_PASSWORD,
 
-    entities: [path.join(__dirname, "entities/*.entity.{.ts, .js}")],
-    migrations: ["./migrations/*{.ts, .js}"]
+    entities: [path.join(__dirname, "entities/*.entity.{ts, js}")],
+    migrations: [path.join(__dirname, "migrations/*{ts, js}")]
 })
 
 export default AppDataSource
